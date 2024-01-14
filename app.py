206ee9c4b27b937325146a7a8aac71a5be35b095
Aastha Mukherjee <53363503+AasthaMuk@users.noreply.github.com>
2024-01-14 13:28:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+# Importing Libraries
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+
+# Main method
+if __name__=="__main__":
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    driver.get("https://www.espncricinfo.com/cricketers/team/india-6")
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+
+    # Wait for images to load
+    # wait = WebDriverWait(driver, 9000)
+    # images = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "img.ds-block")))
+    # images = driver.find_elements(By.CSS_SELECTOR, "img.ds-block")
+    
+    player_names = driver.find_elements(By.CSS_SELECTOR, "span.ds-text-tight-l")
+    age = driver.find_elements(By.CSS_SELECTOR, "span.ds-text-tight-m.ds-font-regular.ds-text-typo-mid3")
+
+    
+    players = [i.text for i in player_names]
+    ages = [i.text.split("y")[0] for i in age]
+
+    player_info={'name':players, 'age':ages}
+    print(player_info)
+
+    pd.DataFrame(player_info).to_csv('player_info.csv')
+
+    # print(len(images))
+    # print(len(player_names))
+    # print(len(age))
+    # for i in range(len(player_names)):
+    #     print(player_names[i].text)
+ 
+    # for i in range(len(age)):
+    #     print(age[i].text)
+    # print(player_names[0].text)
+    # print(type(images))
+        
+    # for i in range(len(images)):
+    #     print(images[i].get_attribute("alt")+" "+images[i].get_attribute("src"))
+    #     # print(images[i].get_attribute("src"))
+
+    
+    # driver.find_element(By.NAME, "q").send_keys("selenium")
+    # driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
---
+++ test.py
@@ -0,0 +1,88 @@
+# Importing Libraries
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+import re
+
+# Main method
+if __name__=="__main__":
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    driver.get("https://www.espncricinfo.com/cricketers/team/india-6")
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+
+    # Wait for images to load
+    # wait = WebDriverWait(driver, 9000)
+    # images = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "img.ds-block")))
+    # images = driver.find_elements(By.CSS_SELECTOR, "img.ds-block")
+    
+    names=[]
+    ages=[]
+    links=[]
+
+    # Scroll down to trigger lazy loading
+    while True:
+        # Your CSS selector for the elements you want to retrieve
+        player_names = driver.find_elements(By.CSS_SELECTOR, 'span.ds-text-tight-l')
+        age = driver.find_elements(By.CSS_SELECTOR, "span.ds-text-tight-m.ds-font-regular.ds-text-typo-mid3")
+        link = driver.find_elements(By.CSS_SELECTOR, 'div.ds-popper-wrapper.ds-inline>a.ds-flex')
+
+        # Scroll down using JavaScript to trigger lazy loading
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Add a wait to allow time for lazy loading to occur
+        time.sleep(2)
+
+        # Break the loop if no more content is loaded
+        if len(driver.find_elements(By.CSS_SELECTOR, 'span.ds-text-tight-l')) == len(player_names):
+            break
+
+        # Break the loop if no more content is loaded
+        if len(driver.find_elements(By.CSS_SELECTOR, 'span.ds-text-tight-m.ds-font-regular.ds-text-typo-mid3')) == len(age):
+            break
+
+        # Break the loop if no more content is loaded
+        if len(driver.find_elements(By.CSS_SELECTOR, 'div.ds-popper-wrapper.ds-inline>a.ds-flex')) == len(link):
+            break
+
+    
+
+    for i in link:
+        pattern = r'https://www\.espncricinfo\.com/cricketers/[a-zA-Z0-9-]+-\d+'
+        if re.match(pattern, str(i.get_attribute('href'))):
+            links.append(i.get_attribute('href'))
+            
+    # print(len(links))
+    # print(links)
+
+    for i in player_names:
+        names.append(i.text)
+    # print(len(names))
+    # print(names)
+
+    
+
+    for i in age:
+        ages.append(i.text)
+    # print(len(ages))
+    # print(ages)
+
+    players_info={'name':names, 'age':ages,'link':links}
+    print(players_info)
+
+    pd.DataFrame(players_info).to_csv('player_info.csv')
+    
+
+    
+    
+    
+    
